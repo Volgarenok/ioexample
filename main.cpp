ef31496f31c46b8a1d69ca2e70037513b5a9fc19
Volgarenok <volgarenok@gmail.com>
2024-03-07 19:02:36 +0300

    Operator overloads

--- main.cpp
+++ main.cpp
@@ -1,18 +1,56 @@
 #include <iostream>
 
+namespace durka
+{
+  struct Data
+  {
+    Data(int aa, int bb):
+     a(aa),
+     b(bb)
+    {}
+    int get_a() const
+    {
+      return a;
+    }
+    int get_b() const
+    {
+      return b;
+    }
+   private:
+    int a, b;
+  };
+
+  std::istream & operator>>(std::istream & in, Data & value)
+  {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+      return in;
+    }
+    int a = 0, b = 0;
+    in >> a >> b;
+    if (in)
+    {
+      value = Data(a, b);
+    }
+    return in;
+  }
+  std::ostream & operator<<(std::ostream & out, const Data & value)
+  {
+    std::ostream::sentry guard(out);
+    if (!guard)
+    {
+      return out;
+    }
+    out << value.get_a() << " " << value.get_b();
+    return out;
+  }
+}
+
 int main()
 {
-  int a = 0, b = 0;
-  if (!(std::cin >> a >> b))
-  {
-    std::cin.clear();
-    std::cin.ignore(1);
-    if (!(std::cin >> a >> b))
-    {
-      std::cerr << "Error\n";
-      return 1;
-    }
-  }
-  std::cout << a << " " << b << "\n";
+  durka::Data d(0, 0);
+  std::cin >> d;
+  std::cout << d << "\n";
   return 0;
 }
